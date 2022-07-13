04beab5cd3493e36f853735eba60547b1f945d9b
archuu2015 <103922955+archuu2015@users.noreply.github.com>
2022-07-13 19:33:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
 names[3]="Jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
 names[9]="Jim";
 
 
